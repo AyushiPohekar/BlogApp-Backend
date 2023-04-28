2c1f6860d54358a1ae0d6f576f076c3e23d45d83
Ayushi Rhutwik Pohekar <ayushi2021.29@gmail.com>
2023-04-28 02:38:39 +0100

    final

--- app.js
+++ app.js
@@ -4,20 +4,26 @@ import dotenv from 'dotenv';
 import userRouter from './routing/user-routes';
 import postRouter from './routing/post-routes';
 import cors from 'cors'
+import connectDB from './config/db';
 const app=express();
 dotenv.config();
 app.use(cors());
-const username=process.env.Db_name;
-const password=process.env.Db_password;
+
+//database config
+connectDB();
 
 //middlewares
 app.use(express.json());
 app.use("/users",userRouter)
 app.use("/posts",postRouter)
-//connections
-mongoose.connect(`mongodb+srv://${username}:${password}@cluster0.pvgxw4g.mongodb.net/blogApp?retryWrites=true&w=majority`
-).then(()=>app.listen(5000,()=>{
-    console.log(`Connection Sucessful and listening to local Port ${5000}`)
-})).catch((error)=>console.log(error));
+
+
+//PORT
+const PORT=process.env.PORT||8010
+
+//RUN LISTEN
+app.listen(PORT,()=>{
+    console.log(`Server running on ${PORT}`)
+})
 
 
--- controllers/user-controllers.js
+++ controllers/user-controllers.js
@@ -16,75 +16,143 @@ export const getAllUsers = async (req, res) => {
   return res.status(200).json({ users });
 };
 
-export const getUserById=async(req,res)=>{
+export const getUserById = async (req, res) => {
   let user;
-  const id=req.params.id;
-  try{
-    user=await User.findById(id).populate("posts")
-  }catch{
-    return console.log(error)
+  const id = req.params.id;
+  try {
+    user = await User.findById(id).populate("posts");
+  } catch {
+    return console.log(error);
   }
   if (!user) {
     return res.status(500).json({ message: "unexected Error occurred" });
   }
 
   return res.status(200).json({ user });
-}
+};
+
+// export const signup = async (req, res, next) => {
+//   const { name, email, password } = req.body;
+//   if (
+//     !name &&
+//     name.trim() === "" &&
+//     !email &&
+//     email.trim() === "" &&
+//     !password &&
+//     password.length < 6
+//   ) {
+//     return res.status(422).json({ message: "Inavalid Data" });
+//   }
+
+//   const hashedPassword = hashSync(password);
+
+//   let user;
+//   try {
+//     const preuser = await User.findOne({ email: email });
+
+//     if (preuser) {
+//       res.status(422).json({ error: "This Email is Already Exist" });
+//     } else {
+//       user = new User({ email, name, password: hashedPassword });
+//       await user.save();
+//       if (!user) {
+//         return res.status(500).json({ message: "Unexpected Error Occured" });
+//       }
+    
+//       return res.status(201).json({ user });
+//     }
+//   } catch (err) {
+//     return console.log(err);
+//   }
+
+
+// };
 
 export const signup = async (req, res, next) => {
-    const { name, email, password } = req.body;
-    if (
-      !name &&
-      name.trim() === "" &&
-      !email &&
-      email.trim() === "" &&
-      !password &&
-      password.length < 6
-    ) {
-      return res.status(422).json({ message: "Inavalid Data" });
-    }
+  const { name, email, password } = req.body;
+  let existingUser;
+  try {
+    existingUser = await User.findOne({ email });
+  } catch (err) {
+    return console.log(err);
+  }
+  if (existingUser) {
+    return res
+      .status(400)
+      .json({ message: "User Already Exists! Login Instead" });
+  }
+  const hashedPassword = hashSync(password);
+
+  const user = new User({
+    name,
+    email,
+    password: hashedPassword,
   
-    const hashedPassword=hashSync(password)
-  
-    let user;
-    try {
-      user = new User({ email, name, password:hashedPassword });
-      await user.save();
-    } catch (err) {
-      return console.log(err);
-    }
-  
-    if (!user) {
-      return res.status(500).json({ message: "Unexpected Error Occured" });
-    }
-  
-    return res.status(201).json({ user });
-  };
+  });
+
+  try {
+    await user.save();
+  } catch (err) {
+    return console.log(err);
+  }
+  return res.status(201).json({ user });
+};
+
+
+// export const signup = async (req, res, next) => {
+//   const { name, email, password } = req.body;
+//   if (
+//     !name &&
+//     name.trim() === "" &&
+//     !email &&
+//     email.trim() === "" &&
+//     !password &&
+//     password.length < 6
+//   ) {
+//     return res.status(422).json({ message: "Inavalid Data" });
+//   }
+
+//   const hashedPassword = hashSync(password);
+
+//   let user;
+//   try {
+//     user = new User({ email, name, password: hashedPassword });
+//     await user.save();
+//   } catch (err) {
+//     return console.log(err);
+//   }
+
+//   if (!user) {
+//     return res.status(500).json({ message: "Unexpected Error Occured" });
+//   }
+
+//   return res.status(201).json({ user });
+// };
 
 
 
 export const login = async (req, res, next) => {
-    const { email, password } = req.body;
-    if (!email && email.trim() === "" && !password && password.length < 6) {
-      return res.status(422).json({ message: "Inavalid Data" });
-    }
-  
-    let existingUser;
-    try {
-      existingUser = await User.findOne({ email });
-    } catch (err) {
-      return console.log(err);
-    }
-    if (!existingUser) {
-      return res.status(404).json({ message: "No user found" });
-    }
-    const isPasswordCorrect = compareSync(password, existingUser.password);
-  
-    if (!isPasswordCorrect) {
-      return res.status(400).json({ message: "Incorrect Password" });
-    }
-  
-    return res
-      .status(200)
-      .json({ id: existingUser._id, message: "Login Successfull" });
-  };
+  const { email, password } = req.body;
+  if (!email && email.trim() === "" && !password && password.length < 6) {
+    return res.status(422).json({ message: "Inavalid Data" });
+  }
+
+  let existingUser;
+  try {
+    existingUser = await User.findOne({ email });
+  } catch (err) {
+    return console.log(err);
+  }
+  if (!existingUser) {
+    return res.status(404).json({ message: "No user found" });
+  }
+  const isPasswordCorrect = compareSync(password, existingUser.password);
+
+  if (!isPasswordCorrect) {
+    return res.status(400).json({ message: "Incorrect Password" });
+  }
+
+  return res
+    .status(200)
+    .json({ id: existingUser._id, message: "Login Successfull" });
+};
